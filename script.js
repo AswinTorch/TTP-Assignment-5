7579aefc73a4e7933e04094d6991ca02204bce7f
Andrea Habib <andrea.atef@yahoo.com>
2020-06-01 16:46:27 -0400

    added remove cells

--- script.js
+++ script.js
@@ -26,5 +26,23 @@ function addNewCells() {
 function removeRow() {
     let rows = document.getElementsByClassName("row");
     tr = Array.from(rows);
+    if (tr.length == 0) {
+        return;
+    }
     tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
+    if(tr.length == 1) {
+        cells = 1;
+    }
+}
+
+function removeCells() {
+    if(cells == 1) {
+        return;
+    }
+    cells--;
+    let rows = document.getElementsByClassName("row");
+    tr = Array.from(rows);
+    for(let i = 0; i < tr.length; i++) {
+        tr[i].removeChild(tr[i].lastChild);
+    }
 }
